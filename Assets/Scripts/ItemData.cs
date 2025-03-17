c77f0ec590c6dc378b7c40adebe31b805e41150e
pinkpurin <nanayean030211@gmail.com>
2025-03-17 11:48:52 +0900

    [Feat] Excel DB 제작 후 Scene에서 로드 까지
    이후 스크립터블 오브젝트를 생성하여 관리

---
+++ Assets/Scripts/ItemData.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[Serializable]
+public class ItemData 
+{
+    public int id;
+    public string itemName;
+    public string description;
+    public string nameEng;
+    public string itemTypeString;
+    [NonSerialized]
+    public ItemType itemType;
+    public int price;
+    public int power;
+    public int level;
+    public bool isStackable;
+    public string iconPath;
+
+    public void InitalizeEnums()
+    {
+        if(Enum.TryParse(itemTypeString, out ItemType parsedType))
+        {
+            itemType = parsedType;
+        }
+        else
+        {
+            Debug.LogError($"아이템 |{itemName}에 유요하지 않은 아이템 타임 : {itemTypeString}");
+            itemType = ItemType.Consumable;
+        }
+    }
+
+}
